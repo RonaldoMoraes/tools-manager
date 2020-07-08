c177682b19e748a095223d06ebb15b65fdaeeacf
Ronaldo <ronaldo.moraes1990@gmail.com>
2020-07-08 12:16:05 -0300

    fix  middleware and validators names

--- app/routes.ts
+++ app/routes.ts
@@ -7,9 +7,9 @@ const routes = express.Router();
 
 const examples = "/examples";
 routes.get(`${examples}`, [middlewares.ExampleMiddleware, validators.ExamplesValidator], controllers.ExamplesController.index);
-// routes.get(`${examples}/:id`, [testingMiddleware.crazy, examplesValidator.index], ExampleController.show);
-// routes.post(`${examples}`, [testingMiddleware.crazy, examplesValidator.index], ExampleController.store);
-// routes.put(`${examples}/:id`, [testingMiddleware.crazy, examplesValidator.index], ExampleController.update);
-// routes.delete(`${examples}/:id`, [testingMiddleware.crazy, examplesValidator.index], ExampleController.destroy);
+// routes.get(`${examples}/:id`, [middlewares.ExampleMiddleware, middlewares.ExampleMiddleware], ExampleController.show);
+// routes.post(`${examples}`, [middlewares.ExampleMiddleware, middlewares.ExampleMiddleware], ExampleController.store);
+// routes.put(`${examples}/:id`, [middlewares.ExampleMiddleware, middlewares.ExampleMiddleware], ExampleController.update);
+// routes.delete(`${examples}/:id`, [middlewares.ExampleMiddleware, middlewares.ExampleMiddleware], ExampleController.destroy);
 
 export default routes;
